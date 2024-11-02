52ce0557086894063ca5a73bd9a881e853f379cc
ZLangJIT <smallville7123@gmail.com>
2024-11-02 14:13:02 +1000

    try build gcc

--- build.sh
+++ build.sh
@@ -10,18 +10,18 @@ TERMUX_PKG_AUTO_UPDATE=false
 TERMUX_PKG_SRCURL=git+https://github.com/riscv-collab/riscv-gnu-toolchain
 TERMUX_PKG_GIT_BRANCH=master
 TERMUX_PKG_HOSTBUILD=true
+TERMUX_PKG_GROUPS="base-devel"
+TERMUX_PKG_HAS_DEBUG=false
 # TERMUX_PKG_RM_AFTER_INSTALL=""
 # TERMUX_PKG_DEPENDS=""
 # TERMUX_PKG_BUILD_DEPENDS=""
 # TERMUX_PKG_CONFLICTS=""
 # TERMUX_PKG_BREAKS=""
 # TERMUX_PKG_REPLACES=""
-TERMUX_PKG_GROUPS="base-devel"
 # TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 # -DANDROID_PLATFORM_LEVEL=$TERMUX_PKG_API_LEVEL
 # --prefix=$(dirname $TERMUX_PREFIX/)
 # "
-TERMUX_PKG_HAS_DEBUG=false
 # termux_step_post_get_source() {
 # 	git fetch --unshallow
 # 	git checkout master
