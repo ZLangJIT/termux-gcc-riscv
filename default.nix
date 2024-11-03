670b5711d43e949243144118ff4aa868a6ca63fb
ZLangJIT <smallville7123@gmail.com>
2024-11-03 16:36:39 +1000

    upload files

--- default.nix
+++ default.nix
@@ -1,33 +1,5 @@
-{ configuration ? import ./configuration.nix, nixpkgs ? <nixpkgs>, extraModules ? [], system ? builtins.currentSystem, platform ? null }:
-
 let
-  pkgs = import nixpkgs {
-  	system = system;
-  	platform = platform;
-  	config = {};
-  };
-
-  mod = rec { 
-  	 _file = ./default.nix;
-    key = _file;
-    config = {
-      nixpkgs.system = pkgs.lib.mkDefault system;
-    };
-  };
-
-  sys = pkgs.lib.evalModules {
-    prefix = [];
-    check = true;
-    modules = [
-      configuration
-      ./base.nix
-      <nixpkgs/nixos/modules/system/boot/kernel.nix>
-      <nixpkgs/nixos/modules/misc/assertions.nix>
-      <nixpkgs/nixos/modules/misc/lib.nix>
-      mod
-    ] ++ extraModules;
-    args = {};
-  };
+  nixpkgs = fetchTarball "https://github.com/NixOS/nixpkgs/tarball/release-24.05";
+  pkgs = import nixpkgs {};
 in
-
-sys.config
+pkgs.pkgsCross.aarch64-android.hello
