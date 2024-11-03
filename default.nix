bd45a221ade5cb18b99c33910207ccbd72e8b9c8
ZLangJIT <smallville7123@gmail.com>
2024-11-03 16:57:01 +1000

    upload files

--- default.nix
+++ default.nix
@@ -1,7 +1,7 @@
 let
   nixpkgs = fetchTarball "https://github.com/NixOS/nixpkgs/tarball/release-24.05";
   pkgs = import nixpkgs {};
-  extra-platforms = aarch64-linux;
-  extra-sandbox-paths = /usr/bin/qemu-user-aarch64;
+  extra-platforms = "aarch64-linux";
+  extra-sandbox-paths = "/usr/bin/qemu-user-aarch64";
 in
 pkgs.pkgsCross.aarch64-android.hello
