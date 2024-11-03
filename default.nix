e3c29647b01314bdf68892f1dad3488c74a2257d
ZLangJIT <smallville7123@gmail.com>
2024-11-03 21:49:58 +1000

    upload files

--- default.nix
+++ default.nix
@@ -1,6 +1,6 @@
 let
   nixpkgs = fetchTarball "https://github.com/NixOS/nixpkgs/tarball/release-24.05";
   host_pkgs = import nixpkgs {};
-  target_pkgs = import nixpkgs { system = "riscv64_rvvm-linux"; };
+  target_pkgs = import nixpkgs { system = "riscv64-rvvm-linux"; };
 in
 target_pkgs.hello
