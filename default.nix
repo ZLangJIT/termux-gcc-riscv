5398ba2b76f7efae2e318e59ee841bd08a4705b2
ZLangJIT <smallville7123@gmail.com>
2024-11-03 18:05:13 +1000

    upload files

--- default.nix
+++ default.nix
@@ -3,7 +3,7 @@ let
   host_nixpkgs = fetchTarball "https://github.com/NixOS/nixpkgs/tarball/release-24.05";
   host_pkgs = import target_nixpkgs {};
   # target packages
-  target_nixpkgs = fetchTarball "https://github.com/NixOS/nixpkgs/tarball/release-24.05" system = aarch64-linux;
-  target_pkgs = import target_nixpkgs {};
+  target_nixpkgs = fetchTarball "https://github.com/NixOS/nixpkgs/tarball/release-24.05";
+  target_pkgs = import target_nixpkgs { system = aarch64-linux };
 in
 target_pkgs.hello
