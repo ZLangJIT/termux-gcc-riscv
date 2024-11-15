bb7333ffd5d507c660c0a20853be72e5bd52a56c
ZLangJIT <smallville7123@gmail.com>
2024-11-16 06:36:06 +1000

    build

--- iso.nix
+++ iso.nix
@@ -15,13 +15,38 @@ with lib;
 
   system.stateVersion = "24.05";
 
-  system.build.bootStage1 = mkForce (pkgs.writeScript "stage1" ''
+    fsInfo =
+      let f = fs: [ fs.mountPoint (if fs.device != null then fs.device else "/dev/disk/by-label/${fs.label}") fs.fsType (builtins.concatStringsSep "," fs.options) ];
+      in pkgs.writeText "initrd-fsinfo" (concatStringsSep "\n" (concatMap f fileSystems));
+
+    setHostId = optionalString (config.networking.hostId != null) ''
+      hi="${config.networking.hostId}"
+      ${if pkgs.stdenv.isBigEndian then ''
+        echo -ne "\x''${hi:0:2}\x''${hi:2:2}\x''${hi:4:2}\x''${hi:6:2}" > /etc/hostid
+      '' else ''
+        echo -ne "\x''${hi:6:2}\x''${hi:4:2}\x''${hi:2:2}\x''${hi:0:2}" > /etc/hostid
+      ''}
+    '';
+
+  bootStage1_script = pkgs.writeScript "stage-1-init" ''
     #!${shell}
     echo
     echo "[1;32m<<< NixOS Stage 1 >>>[0m"
     echo
     exec bash
-  '');
+  '';
+
+  system.build.bootStage1 = mkForce (pkgs.substituteAll {
+    src = ${bootStage1_script}/stage-1-init.sh;
+
+    shell = "${extraUtils}/bin/ash";
+
+    isExecutable = true;
+
+    inherit extraUtils fsInfo setHostId;
+
+    inherit (config.system.nixos) distroName;
+  });
 
   system.build.initialRamdisk = mkForce (pkgs.makeInitrd {
     contents = [ { object = system.build.bootStage1; symlink = "/init"; } ];
