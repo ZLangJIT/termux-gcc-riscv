1369d7c91bcf88d46322a27265f60afbc7064d82
ZLangJIT <smallville7123@gmail.com>
2024-11-16 01:03:58 +1000

    build

--- iso.nix
+++ iso.nix
@@ -1,22 +1,26 @@
-{ config, pkgs, ... }:
+{ config, pkgs, lib, ... }:
+
+with config;
+with lib;
+
 {
   imports = [
     <nixpkgs/nixos/modules/installer/cd-dvd/iso-image.nix>
     <nixpkgs/nixos/modules/installer/cd-dvd/channel.nix>
   ];
 
-  config.boot.kernelParams = [ "console=tty1" "boot.debugtrace" ];
-  config.system.stateVersion = "24.05";
+  boot.kernelParams = [ "console=tty1" "boot.debugtrace" ];
+  system.stateVersion = "24.05";
 
-  config.boot.initrd.availableKernelModules = [ "virtio_net" "virtio_pci" "virtio_mmio" "virtio_blk" "virtio_scsi" "9p" "9pnet_virtio" ];
-  config.boot.initrd.kernelModules = [ "virtio_balloon" "virtio_console" "virtio_rng" "virtio_gpu" ];
+  boot.initrd.availableKernelModules = [ "virtio_net" "virtio_pci" "virtio_mmio" "virtio_blk" "virtio_scsi" "9p" "9pnet_virtio" ];
+  boot.initrd.kernelModules = [ "virtio_balloon" "virtio_console" "virtio_rng" "virtio_gpu" ];
 
-  lib.xdg.autostart.enable = lib.mkDefault false;
-  lib.xdg.icons.enable = lib.mkDefault false;
-  lib.xdg.mime.enable = lib.mkDefault false;
-  lib.xdg.sounds.enable = lib.mkDefault false;
+  xdg.autostart.enable = mkDefault false;
+  xdg.icons.enable = mkDefault false;
+  xdg.mime.enable = mkDefault false;
+  xdg.sounds.enable = mkDefault false;
 
-  config.boot.blacklistedKernelModules = [
+  boot.blacklistedKernelModules = [
     # Obscure network protocols
     "ax25"
     "netrom"
@@ -47,7 +51,7 @@
   ];
 
     # Use less privileged nixos user
-    config.users.users.nixos = {
+    users.users.nixos = {
       isNormalUser = true;
       extraGroups = [ "wheel" "networkmanager" "video" ];
       # Allow the graphical user to login without password
@@ -55,22 +59,22 @@
     };
 
     # Allow the user to log in as root without a password.
-    config.users.users.root.initialHashedPassword = "";
+    users.users.root.initialHashedPassword = "";
 
     # Don't require sudo/root to `reboot` or `poweroff`.
-    config.security.polkit.enable = true;
+    security.polkit.enable = true;
 
     # Allow passwordless sudo from nixos user
-    config.security.sudo = {
+    security.sudo = {
       enable = lib.mkDefault true;
-      wheelNeedsPassword = lib.mkImageMediaOverride false;
+      wheelNeedsPassword = mkImageMediaOverride false;
     };
 
     # Automatically log in at the virtual consoles.
-    config.services.getty.autologinUser = "nixos";
+    services.getty.autologinUser = "nixos";
 
     # Some more help text.
-    config.services.getty.helpLine = ''
+    services.getty.helpLine = ''
       The "nixos" and "root" accounts have empty passwords.
 
       To log in over ssh you must set a password for either "nixos" or "root"
@@ -80,24 +84,24 @@
       If you need a wireless connection, type
       `sudo systemctl start wpa_supplicant` and configure a
       network using `wpa_cli`. See the NixOS manual for details.
-    '' + lib.optionalString config.services.xserver.enable ''
+    '' + optionalString services.xserver.enable ''
 
       Type `sudo systemctl start display-manager' to
       start the graphical user interface.
     '';
 
     # allow nix-copy to live system
-    config.nix.settings.trusted-users = [ "root" "nixos" ];
+    nix.settings.trusted-users = [ "root" "nixos" ];
 
     # Prevent installation media from evacuating persistent storage, as their
     # var directory is not persistent and it would thus result in deletion of
     # those entries.
-    config.environment.etc."systemd/pstore.conf".text = ''
+    environment.etc."systemd/pstore.conf".text = ''
       [PStore]
       Unlink=no
     '';
 
-    config.networking.firewall.logRefusedConnections = lib.mkDefault false;
+    networking.firewall.logRefusedConnections = mkDefault false;
 
 
   #config.nixpkgs.hostPlatform = "riscv64-linux";
