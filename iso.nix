492ea9c5b9c36b375f4c13606d957626ef2bec57
ZLangJIT <smallville7123@gmail.com>
2024-11-15 19:37:34 +1000

    build

--- iso.nix
+++ iso.nix
@@ -3,8 +3,8 @@
   imports = [
     #<iso-base.nix>
     #<channel.nix>
-    <./nixpkgs/nixos/modules/installer/cd-dvd/iso-image.nix>
-    <./nixpkgs/nixos/modules/installer/cd-dvd/channel.nix>
+    <nixpkgs/nixos/modules/installer/cd-dvd/iso-image.nix>
+    <nixpkgs/nixos/modules/installer/cd-dvd/channel.nix>
   ];
   #config.nixpkgs.hostPlatform = "riscv64-linux";
 }
