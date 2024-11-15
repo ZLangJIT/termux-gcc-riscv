1fef9a672faec8a75373fdf4af153de05dfb9c15
ZLangJIT <smallville7123@gmail.com>
2024-11-15 11:12:02 +1000

    build

--- iso.nix
+++ iso.nix
@@ -1,12 +1,16 @@
 { config, pkgs, ... }:
 {
+  nixpkgs.crossSystem = {
+    system = "riscv64-linux";
+  };
+
   imports = [
+    <nixpkgs/nixos/modules/profiles/minimal.nix>
     <nixpkgs/nixos/modules/installer/cd-dvd/installation-cd-minimal.nix>
 
     # Provide an initial copy of the NixOS channel so that the user
     # doesn't need to run "nix-channel --update" first.
     <nixpkgs/nixos/modules/installer/cd-dvd/channel.nix>
   ];
-  environment.systemPackages = [ pkgs.neovim ];
   #isoImage.squashfsCompression = "gzip -Xcompression-level 1";
 }
