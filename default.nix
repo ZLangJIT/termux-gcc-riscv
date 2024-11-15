584d8ba6b6dbe1328eebe1fc42160cbe952e8f9b
ZLangJIT <smallville7123@gmail.com>
2024-11-15 18:02:01 +1000

    build

--- default.nix
+++ default.nix
@@ -1,6 +1,6 @@
 let
   #nixpkgs = fetchTarball "https://github.com/NixOS/nixpkgs/tarball/release-24.05";
-  pkgs = import nixpkgs {
+  pkgs = import <nixpkgs> {
     crossSystem = "riscv64-linux";
   };
 in pkgs.pkgsCross.riscv64.stdenv
