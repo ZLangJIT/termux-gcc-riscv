f8298b8e487374903b8fa2c9ca03a5f5c59e04fd
ZLangJIT <smallville7123@gmail.com>
2024-11-16 00:15:46 +1000

    build

--- iso.nix
+++ iso.nix
@@ -5,7 +5,7 @@
     <nixpkgs/nixos/modules/installer/cd-dvd/channel.nix>
   ];
 
-  config.boot.kernelParams = [ "console=tty1", "boot.debugtrace" ];
+  config.boot.kernelParams = "console=tty1 boot.debugtrace";
   config.system.stateVersion = "24.05";
 
   #config.nixpkgs.hostPlatform = "riscv64-linux";
@@ -14,6 +14,7 @@
   # nixpkgs/nixos/modules/config/system-path.nix
 
   # systemd units are controlled via
+  # nixpkgs/nixos/modules/system/boot/systemd.nix
   # nixpkgs/nixos/modules/system/boot/systemd/initrd.nix
 
 }
